913f0a6e03a94b5415e872c877a7d27e0de3bfdd
bausscucco <jonathanrlouie@gmail.com>
2015-08-04 21:22:23 -0700

    Turned the echo server into a chat server.

--- src/main/scala/ca/jonrlouie/RPSServer.scala
+++ src/main/scala/ca/jonrlouie/RPSServer.scala
@@ -5,13 +5,17 @@ import akka.io.{ IO, Tcp }
 import akka.util.ByteString
 import java.net.InetSocketAddress
 
+case class AddConnection(connection: ActorRef)
+
 class RPSServer extends Actor {
      
   import Tcp._
   import context.system
      
   IO(Tcp) ! Bind(self, new InetSocketAddress("localhost", 9001))
-     
+  
+  val handler = context.actorOf(Props[SimplisticHandler])
+  
   def receive = {
     case b @ Bound(localAddress) =>
       // do some logging or setup ...
@@ -20,8 +24,10 @@ class RPSServer extends Actor {
     case CommandFailed(_: Bind) => context stop self
      
     case c @ Connected(remote, local) =>
-      val handler = context.actorOf(Props[SimplisticHandler])
       val connection = sender()
+      handler ! AddConnection(connection)
+    
+    case ConnectionAdded(connection) =>
       connection ! Register(handler)
   }
      
--- src/main/scala/ca/jonrlouie/SimplisticHandler.scala
+++ src/main/scala/ca/jonrlouie/SimplisticHandler.scala
@@ -5,12 +5,20 @@ import akka.io.{ IO, Tcp }
 import akka.util.ByteString
 import java.net.InetSocketAddress
 
+case class ConnectionAdded(connection: ActorRef)
+
 class SimplisticHandler extends Actor {
   import Tcp._
+    
+  var connections = Nil: List[ActorRef]
+  
   def receive = {
+    case AddConnection(connection) => 
+      connections ::= connection
+      sender() ! ConnectionAdded(connection)
     case Received(data) => 
       println(data.decodeString("UTF-8"))
-      sender() ! Write(data)
+      connections.foreach(_ ! Write(data))
     case PeerClosed => context stop self
   }
 }
